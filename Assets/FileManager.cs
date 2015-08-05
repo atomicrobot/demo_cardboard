eae49f46a6e254e614ae04200167ede4b1385b8e
Stuart Bowman <stuartsoft2012@gmail.com>
2015-08-05 08:04:25 -0400

    * Began adding a file manager to load music from the device's local music folder

---
+++ Assets/FileManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class FileManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string path = Directory.GetCurrentDirectory () + "sdcard/media";
+
+		Debug.Log ("PWD: " + path);
+
+		DirectoryInfo dir = new DirectoryInfo(path);
+		FileInfo[] info = dir.GetFiles("*.*");
+		foreach (FileInfo f in info) 
+		{
+			Debug.Log(f.FullName);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
